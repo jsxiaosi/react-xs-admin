13db953e22f4cd6c315b0d274776dd0dcfd53f00
小斯（xiaosi） <1531733886@qq.com>
2022-12-14 21:40:59 +0800

    feat: ✨ add @jsxiaosi/eslint-config-react

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,19 +6,8 @@ module.exports = {
     es6: true,
     node: true,
   },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
 
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    '@jsxiaosi/eslint-config-ts',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['react', 'react-hooks'],
+  extends: ['@jsxiaosi/eslint-config-react', 'plugin:prettier/recommended'],
   rules: {
     'prettier/prettier': [
       'error',
@@ -26,26 +15,8 @@ module.exports = {
         usePrettierrc: true,
       },
     ],
-    'react/display-name': 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/state-in-constructor': 0,
-    'react/static-property-placement': 0,
-    // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-    'react/destructuring-assignment': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/no-array-index-key': 'warn',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'off',
-    'react/require-default-props': 0,
-    'react/jsx-fragments': 0,
-    'react/jsx-wrap-multilines': 0,
-    'react/prop-types': 0,
-    'react/forbid-prop-types': 0,
-    'react/sort-comp': 0,
-    'react/react-in-jsx-scope': 0,
-    'react/jsx-one-expression-per-line': 0,
-    'react/self-closing-comp': 1,
-    'react/jsx-key': 1,
+
+    // 禁止使用未知的dom属性
     'react/no-unknown-property': ['error', { ignore: ['css'] }],
   },
 };
